730fb79f329ebccf70048dc402ec6de93b1b9cad
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-31 19:15:20 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-app.config.globalProperties.window = window;
+//전역변수
+app.config.globalProperties.contentHtml = window.contentHtml;
+app.config.globalProperties.session = window.session;
+app.config.globalProperties.page = window.page;
+app.config.globalProperties.CONFIG = window.CONFIG;
 
 app.mount('.Vuejs')
