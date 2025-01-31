1ec946e912067ee8753f998bc659f1f85c5da1d5
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-31 10:21:40 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <header>
-    <h1>hi</h1>
+    <nav>
+      <a href="/">{{ window.CONFIG.logo_text }}</a>
+    </nav>
   </header>
+  <main>
+    <div v-html="window.contentHtml"></div>
+  </main>
+  <footer>
+    <p>{{ window.CONFIG.copyright_text }}</p>
+    <a href="//testwiki.hyonsu.com">the tree</a>
+  </footer>
 </template>
 
 <style scoped>
 header {
   height: 64px;
   background-color: blue;
+}
+nav {
+  display: flex;
+}
+main {
+  margin: 5px;
 }
 </style>
