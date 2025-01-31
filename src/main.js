a19e53f2b3dfc8031370809ae19d218e60747d81
wikiengine <157791081+wikiengine@users.noreply.github.com>
2025-01-31 19:24:36 +0900

    반응형

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
 //전역변수
-app.config.globalProperties.contentHtml = window.contentHtml;
+app.config.globalProperties.contentHtml = reactive(window.contentHtml);
+app.config.globalProperties.page = reactive(window.page);
+//반응형 필요없음
 app.config.globalProperties.session = window.session;
-app.config.globalProperties.page = window.page;
 app.config.globalProperties.CONFIG = window.CONFIG;
 
 app.mount('.Vuejs')
